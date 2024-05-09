bf8695cf4c7e80f161498c662f0cd5ada3b34894
Filassii <mateusfilassii@gmail.com>
2024-05-09 15:55:40 -0300

    Update exercicio_1.py

--- condicionais/exercicio_1.py
+++ condicionais/exercicio_1.py
@@ -1,10 +1,40 @@
 import json
 import urllib.request
-cep=input('Digite o CEP: ')
-url= f"https://viacep.com.br/ws/{cep}/json/"
+
+# Solicitar ao usuário o CEP
+cep = input("Digite o CEP: ")
+
+# Conatruir na URL de consulta
+url = f"https://viacep.com.br/ws/{cep}/json/"
+
 try:
-    response=urllib.request.urlopen(url)
-    data=response.read().decode('utf-8')
-    print(data)
+    # Fazer a requisição
+    response = urllib.request.urlopen(url)
+
+    # Ler o conteúdo da resposta
+    data = response.read().decode("utf-8")
+
+    #Converter JSON em dicionário Pythin
+    endereco = json.loads(data)
+
+    #Verificar se a consulta foi bem-sucedida
+    if endereco.get("erro"):
+        print("CEP não encontrado")
+    else:
+        #Armazenar informaçoes em variáveis
+        logradouro = endereco["logradouro"] 
+        complemento = endereco["complemento"] 
+        bairro = endereco["bairro"]  
+        cidade = endereco["localidade"]
+        estado = endereco["uf"]
+
+        #Exibir informações do endereço na tela
+        print(f"Logradouro: {logradouro}")
+        print(f"Complemento: {complemento}")
+        print(f"Bairro: {bairro}")
+        print(f"Cidade: {cidade}")
+        print(f"Estado: {estado}")
+    
+    #Fechar a conexão
 except Exception as e:
-    print(f'Erro: {e}')     
+    print(f"Erro: {e}")
